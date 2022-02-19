7d5d31335ec902427db3707b3fa859ce26abffe2
Md Nazmul Hasan <nazmul.ns7989@gmail.com>
2022-02-19 16:10:04 +0600

    post student info done

--- resources/js/components/AllData.js
+++ resources/js/components/AllData.js
@@ -1,31 +1,54 @@
 import React, {Fragment,useState,useEffect} from 'react';
+import Swal from 'sweetalert2'
 
 
 const AllData = () => {
 
+    //add new student info
     const [info,setInfo] = useState({
         name: '',
         age: '',
-        class: ''
+        class: '',
     })
 
+    //validation error
+    const [error,setError] = useState({
+        error_list: []
+    })
+
+    //handle input value
     const onChangeHandler = (e)=>{
         let inputName = e.target.name;
         let inputValue = e.target.value;
 
-        setInfo({
-            [inputName]: inputValue
-        })
+        setInfo(prev=>({...prev,[inputName]: inputValue}))
     }
 
-    const submitHandler = (e)=>{
+
+    //submit form data
+    const submitHandler = async (e)=>{
         e.preventDefault();
 
-        const res = axios.post('/api/student/store',info);
+        const res = await axios.post('/api/student/store',info);
 
-        if (res.data === 200){
+        if (res.data.status === 200){
 
-            console.log(res.data)
+            const Toast = Swal.mixin({
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                    toast.addEventListener('mouseenter', Swal.stopTimer)
+                    toast.addEventListener('mouseleave', Swal.resumeTimer)
+                }
+            })
+
+            Toast.fire({
+                icon: 'success',
+                title: 'Student Info Added successfully'
+            })
 
             setInfo({
                 name: '',
@@ -35,15 +58,20 @@ const AllData = () => {
 
         }else {
 
+            setError({
+                error_list: res.data.validation_error
+            });
+
         }
 
     }
 
-
+    //set data from fetched student info
     const [students,setStudents] = useState({
         infos: []
     });
-    
+
+    //fetch all student info
     const getStudents = ()=>{
           axios.get('/api/student').then((response)=>{
             let studentsData = response.data.students;
@@ -56,11 +84,13 @@ const AllData = () => {
         })
     }
 
+    //call the get info function when app is mounted
     useEffect(()=>{
         getStudents();
     },[]);
 
 
+    //looping the fetched students info
     const studentData = students.infos.map((student)=>{
         return <tr key={student.id}>
             <td>{student.id}</td>
@@ -121,19 +151,19 @@ const AllData = () => {
                                     <div className="col-12">
                                         <div className="form-group mb-3">
                                             <input type="text" name="name" onChange={onChangeHandler} value={info.name}  id="name" className="form-control" placeholder="Your Name"/>
-                                            {/*<span className="text-danger">{this.state.error_list.name}</span>*/}
+                                            <span className="text-danger">{error.error_list.name}</span>
                                         </div>
                                     </div>
                                     <div className="col-12">
                                         <div className="form-group mb-3">
                                             <input type="text" name="age" id="age" onChange={onChangeHandler} value={info.age}  className="form-control" placeholder="Your Age"/>
-                                            {/*<span className="text-danger">{this.state.error_list.age}</span>*/}
+                                            <span className="text-danger">{error.error_list.age}</span>
                                         </div>
                                     </div>
                                     <div className="col-12">
                                         <div className="form-group mb-3">
                                             <input type="text" name="class" id="class" onChange={onChangeHandler} value={info.class}  className="form-control" placeholder="Your Class"/>
-                                            {/*<span className="text-danger">{this.state.error_list.class}</span>*/}
+                                            <span className="text-danger">{error.error_list.class}</span>
                                         </div>
                                     </div>
 
